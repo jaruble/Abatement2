f7d03af972e5a546a7ce82750ceb7b2e924a510d
jaruble <jeremy.a.ruble@gmail.com>
2025-05-29 10:43:59 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/";
+import { Input } from "@/components/ui/";
+import { Card, CardContent } from "@/components/ui/";
 
 const categories = [
   {
